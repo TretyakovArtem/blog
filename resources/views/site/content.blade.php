7be19c6dd8263006533f579cae9a4374f69d0c0f
a_tretyakov <artem.tretyakov.91@inbox.ru>
2017-11-28 10:46:21 +0300

    fix

--- resources/views/site/content.blade.php
+++ resources/views/site/content.blade.php
@@ -14,7 +14,7 @@
                                         </div>
                                     </div>
                                     <div class="content-footer">
-                                        <span style="font-size: 16px;color: #fff;">Sumit Kumar</span>
+                                        <span style="font-size: 16px;color: #fff;">PHP</span>
                                         <span class="pull-right">
                                             <a href="#" data-toggle="tooltip" data-placement="left" title="Comments"><i class="fa fa-comments" ></i> 30</a>
                                             <a href="#" data-toggle="tooltip" data-placement="right" title="Loved"><i class="fa fa-heart"></i> 20</a>
