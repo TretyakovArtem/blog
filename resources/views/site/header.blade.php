0faf50ae22db60093be7971b187bac6a9f25205b
a_tretyakov <artem.tretyakov.91@inbox.ru>
2018-01-30 09:03:25 +0300

    fix

--- resources/views/site/header.blade.php
+++ resources/views/site/header.blade.php
@@ -6,11 +6,10 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#"><h5>PATHFINDER</h5></a>
+            <a class="navbar-brand" href="{{ route('posts') }}"><h5>PATHFINDER</h5></a>
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
-                <li><a href="{{ route('posts') }}">Home</a></li>
                 <li><a href="{{ route('contacts') }}">Контакты</a></li>
                 <li><a href="https://github.com/TretyakovArtem">GitHub</a></li>
             </ul>
